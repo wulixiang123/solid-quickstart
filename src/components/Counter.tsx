759561cd963f6284d473251fa7cc8f381ea1f0a0
liuyuanbo <1131107655@qq.com>
2023-06-17 21:24:15 +0800

    更新

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,7 +5,7 @@ export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
     <>
-    <img src="/src/images/01.jpg" alt="" />
+    <img src="/src/images/11.jpg" alt="" />
     <button class="increment" onClick={() => setCount(count() + 1)}>
       Clicks: {count()}
     </button>
