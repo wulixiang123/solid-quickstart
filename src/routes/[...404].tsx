9806711ea295c140c7f351ce5f00be0c2b44edb5
liuyuanbo <1131107655@qq.com>
2023-06-17 11:31:21 +0800

    更新

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -8,11 +8,7 @@ export default function NotFound() {
       <HttpStatusCode code={404} />
       <h1>Page Not Found</h1>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        哦吼
       </p>
     </main>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,11 +8,6 @@ export default function Home() {
       <h1>Hello world!</h1>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
         qq:1131107655
       </p>
     </main>
